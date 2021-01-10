6c2b1fbced8a96726f98f5ba8a4395f3de2ac5fd
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-01-09 20:08:40 -0600

    Added basic styling

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ const express = require('express');
 app = express();
 
 const port = 3000
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
