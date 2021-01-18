1639cfb935068903f91143d340db6de0b1c8efd7
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-01-17 19:58:45 -0600

    Added image loading and changing functions

--- public/index.js
+++ public/index.js
@@ -1,29 +1,58 @@
-var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d")
+let sampleData = [{
+    url:"img/road.jpg",
+    title:"Autumn Road",
+    description:"Taken during the fall, somewhere in the backroads not too far from town."
+},{
+    url:"img/sea.jpg",
+    title:"Seaside",
+    description:"A pretty day on the beach, nothing like it."
+}]
 
-function resizeCanvas(){
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
+let currentImage = 0;
+let data = [];
+let target = document.getElementById("target")
+let loaderGif = document.getElementById("loaderImage")
+let title = document.getElementById("imageTitle")
+let description = document.getElementById("imageDescription")
+
+function load(input){
+    data = input;
+    updateImage()
 }
 
-var testImage = new Image()
-testImage.src = "img/road.jpg"
-testImage.onload = ()=>{
-    console.log("loaded")
-    resizeCanvas()
-    main()
+function updateImage(){
+    target.src = data[currentImage].url;
+    loaderToggle()
+    target.onload = (e)=>{
+        title.innerHTML = data[currentImage].title
+        description.innerHTML = data[currentImage].description
+        loaderToggle()
+    }
 }
 
-function main(){
-    //test
-
-    context.drawImage(
-        testImage,
-        0,
-        0,
-        canvas.width,
-        canvas.height
-    )
+function loaderToggle(){
+    loaderGif.classList.toggle('invisible')
 }
 
-document.addEventListener("resize",resizeCanvas)
+function next(){
+    currentImage++;
+    updateImage()
+}
+
+function prev(){
+    currentImage--;
+    updateImage()
+}
+
+load(sampleData)
+
+
+document.getElementById("upButton").onclick = (e)=>{
+    e.preventDefault();
+    next();
+}
+
+document.getElementById("downButton").onclick = (e)=>{
+    e.preventDefault();
+    prev();
+}
